1480e1e21e90f39cd078ef7c72627dc7e54ea368
Awesome YAMAUCHI <torifuku.kaiou@gmail.com>
2025-12-23 07:46:04 +0900

    Initial commit

---
+++ test/alb-cognito-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AlbCognitoDemo from '../lib/alb-cognito-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/alb-cognito-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AlbCognitoDemo.AlbCognitoDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
